f70f4aadc90a104dec3e434b9d790f319e2a220a
gustalicht <90413439+gustalicht@users.noreply.github.com>
2023-06-08 00:42:23 -0300

    mais um teste

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Teste de git pull do Gustavo")
 console.log("Teste Dentro do VsCode")
+
+
+console.log("TESTE DE GIT PULL APENAS")
